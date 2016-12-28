8609099affe2b000953eecd7447673d422b9744d
mingin2007 <mingin2007@github.com>
2016-12-28 11:19:30 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("#left-menu")
+      .addEventListener("click", function () {
+
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("/snippets/home.html", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#left-menu")
+                .innerHTML = name;
+            });
+
+        
+      });
+  }
+);
+
+
+
+
